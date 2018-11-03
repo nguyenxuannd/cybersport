efdac44eeb314a4603bee294a60422b75a5d109f
Nguyen Dinh Xuan <Xuan.nguyendinh@ncc.asia>
2018-11-03 08:27:54 +0700

    add Player entity

---
+++ src/main/java/com/example/cybersport/entities/Player.java
@@ -0,0 +1,29 @@
+package com.example.cybersport.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+		
+public class Player {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+//	private String image;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
